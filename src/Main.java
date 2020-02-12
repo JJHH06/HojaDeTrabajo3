c35498596d7d377ca37d2bd52e44b54a41e32370
Jose Hurtarte <JJHH121106@gmail.com>
2020-02-12 16:35:46 -0600

    xd

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * 
  */
@@ -13,7 +15,17 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Random rand = new Random();
+		
+		int[] numeros = new int[8];
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = rand.nextInt(100); 
+		}
+		
+		for (int i : numeros) {
+			System.out.println(i);
+		}
+		
 	}
 
 }
